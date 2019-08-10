5fa24072160417db0a25e376b9b32ced22cbf1bd
pamu47 <pamudithamaduranga@gmail.com>
2019-08-11 01:35:07 +0530

    Drawer and Dashboard added

---
+++ lib/dashboard.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class Dashboard extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return DashboardState();
+  }
+}
+
+class DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StaggeredGridView.count(
+        crossAxisCount: 2,
+        crossAxisSpacing: 12,
+        mainAxisSpacing: 12.0,
+        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        children: <Widget>[
+          myItems(FontAwesomeIcons.userMd, "Doctors", 0xffed622b),
+          myItems(FontAwesomeIcons.procedures, "Patients", 0xff26cb3c),
+          myItems(Icons.notifications, "Notification", 0xffff3266),
+          myItems(Icons.attach_money, "Balance", 0xff3399fe),
+          myItems(Icons.settings, "Settings", 0xfff4c83f),
+          myItems(Icons.group_work, "Group work", 0xff622F74),
+          myItems(Icons.favorite, "Followers", 0xffad61f1),
+          myItems(Icons.message, "Messages", 0xff7297ff),
+        ],
+        staggeredTiles: [
+          StaggeredTile.extent(2, 130.0),
+          StaggeredTile.extent(1, 250.0),
+          StaggeredTile.extent(1, 130.0),
+          StaggeredTile.extent(1, 130.0),
+          StaggeredTile.extent(1, 150.0),
+          StaggeredTile.extent(1, 130.0),
+          StaggeredTile.extent(2, 200.0),
+          StaggeredTile.extent(2, 120.0),
+        ],
+      ),
+    );
+  }
+
+  Material myItems(IconData icon, String heading, int color) {
+    return Material(
+      color: Colors.white,
+      elevation: 14.0,
+      shadowColor: Color(0x802196F3),
+      borderRadius: BorderRadius.circular(24.0),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+
+
+
+                    Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          heading,
+                          style: TextStyle(
+                            color: new Color(color),
+                            fontSize: 20.0
+                          ),
+                        ),
+                      ),
+
+
+                  Material(
+                    color: new Color(color),
+                    borderRadius: BorderRadius.circular(24.0),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Icon(
+                        icon,
+                        color: Colors.white,
+                        size: 30.0,
+                        ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/homescreen.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+import 'dashboard.dart';
+
+
+// void main() {
+//   runApp();
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: "Flutter Demo",
+//       theme: ThemeData(primaryColor: new Color(0xff622F74)),
+//       home: Dashboard(),
+//     );
+//   }
+// }
+
+
+
+class MyApp extends StatelessWidget {
+  final appTitle = 'Dashboard';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        '/first':(context)=>MyApp(),
+      },
+      debugShowCheckedModeBanner: false,
+      title: appTitle,
+      home: MyHomePage(title: appTitle),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: Dashboard(),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              currentAccountPicture: Icon(Icons.account_circle, size:75.0),
+              accountEmail: Text("pamuditha@gmail.com"),
+              accountName: Text("Pamuditha"),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              
+            ),
+            Card(
+            child: ListTile(
+              leading: Icon(Icons.add_location),
+              title: Text('Location'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),),
+            Card(
+              child:ListTile(
+              leading: Icon(Icons.add_shopping_cart),
+              title: Text('Shopping'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),)
+          ],
+        ),
+      ),
+    );
+  }
+}
