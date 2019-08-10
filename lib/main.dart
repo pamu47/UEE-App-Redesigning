405d3f6fb8d3c02ff3846f5223d379c7cf9abb6b
pamu47 <pamudithamaduranga@gmail.com>
2019-08-11 01:34:10 +0530

    Splash screen added

---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:hospital_mgt/homescreen.dart';
+
+void main() => runApp(new MaterialApp(
+      routes: {
+        '/home': (context)=>MyApp()
+      },
+      theme: ThemeData(primaryColor: Colors.white,accentColor: Colors.white),
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    ));
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds:5), ()=> Navigator.pushNamed(context, '/home'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(color: Colors.white),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                flex: 3,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      getAssetImage(),
+                      Text(
+                        "Hospital Management System",
+                        style: TextStyle(
+                            fontSize: 20.0, 
+                            fontWeight: FontWeight.bold
+                            ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    CircularProgressIndicator(backgroundColor: Colors.red,),
+                    Padding(
+                      padding: EdgeInsets.only(top:10.0),
+                    ),
+                    //Text("CodeWarriors")
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget getAssetImage() {
+    AssetImage assetImage = AssetImage('images/heart.png');
+    Image image = Image(
+      image: assetImage,
+      width: 100.0,
+      height: 100.0,
+    );
+    return Container(child: image);
+  }
+}
