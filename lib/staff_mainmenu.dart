14e1be7470525a919f1d412b80e41667820eacc7
Haritha Weerathunga <39055744+HarithaWeerathunga@users.noreply.github.com>
2019-08-11 14:39:48 +0530

    Staff Menu Page
    Still not configured to the main dashboard

---
+++ lib/staff_mainmenu.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class StaffMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Staff Page'),
+      ),
+      body: Center(
+        child: RaisedButton(
+            onPressed: (){}
+        ),
+      ),
+    );
+  }
+}
