5aadf557956f085e27554808ef1a7b96101a6027
pamu47 <pamudithamaduranga@gmail.com>
2019-08-14 01:32:28 +0530

    updated tiles for navigation

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:hospital_mgt/patients.dart';
 
 class Dashboard extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
     return DashboardState();
   }
 }
@@ -20,14 +20,14 @@ class DashboardState extends State<Dashboard> {
         mainAxisSpacing: 12.0,
         padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
         children: <Widget>[
-          myItems(FontAwesomeIcons.userMd, "Doctors", 0xffed622b),
-          myItems(FontAwesomeIcons.procedures, "Patients", 0xff26cb3c),
-          myItems(Icons.notifications, "Noticeboard", 0xffff3266),
-          myItems(Icons.message, "Messages", 0xff3399fe),
-          myItems(Icons.payment, "Payments", 0xfff4c83f),
-          myItems(FontAwesomeIcons.pills, "Medicine", 0xff622F74),
-          myItems(Icons.favorite, "Followers", 0xffad61f1),
-          myItems(Icons.message, "Messages", 0xff7297ff),
+          myItems(FontAwesomeIcons.userMd, "Doctors", 0xffed622b, () {}),
+          myItems(FontAwesomeIcons.procedures, "Patients", 0xff26cb3c, Patients()),
+          myItems(Icons.notifications, "Noticeboard", 0xffff3266, () {}),
+          myItems(Icons.message, "Messages", 0xff3399fe, () {}),
+          myItems(Icons.payment, "Payments", 0xfff4c83f, () {}),
+          myItems(FontAwesomeIcons.pills, "Medicine", 0xff622F74, () {}),
+          myItems(Icons.favorite, "Followers", 0xffad61f1, () {}),
+          myItems(Icons.message, "Messages", 0xff7297ff, () {}),
         ],
         staggeredTiles: [
           StaggeredTile.extent(2, 130.0),
@@ -43,54 +43,52 @@ class DashboardState extends State<Dashboard> {
     );
   }
 
-  Material myItems(IconData icon, String heading, int color) {
+  Material myItems(IconData icon, String heading, int color, method) {
     return Material(
-      color: Colors.white,
-      elevation: 14.0,
-      shadowColor: Color(0x802196F3),
-      borderRadius: BorderRadius.circular(24.0),
-      child: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Column(
+        color: Colors.white,
+        elevation: 14.0,
+        shadowColor: Color(0x802196F3),
+        borderRadius: BorderRadius.circular(24.0),
+        child: InkWell(
+          onTap: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => method));
+          },
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-
-
-
-                    Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          heading,
-                          style: TextStyle(
-                            color: new Color(color),
-                            fontSize: 20.0
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            heading,
+                            style: TextStyle(
+                                color: new Color(color), fontSize: 20.0),
+                          ),
+                        ),
+                      Material(
+                        color: new Color(color),
+                        borderRadius: BorderRadius.circular(24.0),
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Icon(
+                            icon,
+                            color: Colors.white,
+                            size: 30.0,
                           ),
                         ),
                       ),
-
-
-                  Material(
-                    color: new Color(color),
-                    borderRadius: BorderRadius.circular(24.0),
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Icon(
-                        icon,
-                        color: Colors.white,
-                        size: 30.0,
-                        ),
-                    ),
+                    ],
                   )
                 ],
-              )
-            ],
+              ),
+            ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
