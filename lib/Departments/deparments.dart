216a8c8c4f5d6e9a150758ba3197d84df54711f5
pamu47 <pamudithamaduranga@gmail.com>
2019-08-30 02:03:32 +0530

    form for add department almost done

--- lib/Departments/deparments.dart
+++ lib/Departments/deparments.dart
@@ -10,6 +10,26 @@ class Departments extends StatefulWidget {
 }
 
 class DepartmentState extends State<Departments> {
+  int groupValue;
+  bool b = true;
+
+  @override
+  void initState() {
+    super.initState();
+    groupValue = 0;
+  }
+
+  setSelectedRadio(int val) {
+    setState(() {
+      groupValue = val;
+      if (val == 0) {
+        b = true;
+      } else if (val == 1) {
+        b = false;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -46,11 +66,81 @@ class DepartmentState extends State<Departments> {
                     width: 400,
                     //padding: new EdgeInsets.all(120.0),
                     child: new ListView(
-                      padding: EdgeInsets.all(20.0),
+                      padding: EdgeInsets.all(30.0),
                       children: <Widget>[
-                        Container(height: 75,),
-                        CustomTextField("Department Name", "abc"),
-                        CustomTextField("Department Name", "abc")
+                        Container(
+                          height: 40,
+                        ),
+                        CustomTextField("Department Name", "Cardiology/Diagnostic Imaging"),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(right: 2.0, left: 2.0),
+                              child: Card(
+                                elevation: 0.2,
+                                child: ListTile(
+                                  title: Text(
+                                    "Department type.......",
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                new Radio(
+                                  activeColor: Colors.green,
+                                  value: 0,
+                                  groupValue: groupValue,
+                                  onChanged: (val) {
+                                    setSelectedRadio(val);
+                                  },
+                                ),
+                                new Text(
+                                  'Wards',
+                                  style: new TextStyle(fontSize: 16.0),
+                                ),
+                                new Radio(
+                                  activeColor: Colors.red,
+                                  value: 1,
+                                  groupValue: groupValue,
+                                  onChanged: (val) {
+                                    setSelectedRadio(val);
+                                  },
+                                ),
+                                new Text(
+                                  'Clinic',
+                                  style: new TextStyle(fontSize: 16.0),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              bottom: 12.0, right: 8.0, left: 8.0),
+                          child: Card(),
+                        ),
+                        b
+                            ? CustomTextField("Number of beds", "25")
+                            : Container(),
+                        ButtonTheme(
+                          minWidth: 200.0,
+                          child: RaisedButton(
+                            color: Colors.green,
+                            shape: RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(8.0))),
+                            child: Text("Done"),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ),
                       ],
                     ),
                   ),
